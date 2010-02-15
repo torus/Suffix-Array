3bb56a534d053f9382c64aa4af077bf8572d02b5
Toru Hisai <toru-github@torus.jp>
2010-02-14 21:52:25 -1000

    SWIG invoked from Xcode

---
+++ xcode/malloc.h
@@ -0,0 +1,10 @@
+/*
+ *  malloc.h
+ *  Chapter3 Framework
+ *
+ *  Created by Toru Hisai on 10/02/14.
+ *  Copyright 2010 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+// DUMMY FILE TO AVOID COMPILE ERROR
