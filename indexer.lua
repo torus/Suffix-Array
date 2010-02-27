fed9df52ec49b3c8c0cebd8848bf7e1019839c55
Toru Hisai <toru-github@torus.jp>
2010-02-27 00:16:59 -1000

    fixed case-related problems.  changed function name

--- indexer.lua
+++ indexer.lua
@@ -11,6 +11,29 @@ function loadindex (idxfile_, srcfile)
    -- idxfile = idxfile_
 end
 
+function get_str_pos (idxf, m)
+   idxf:seek ("set", (m - 1) * 4)
+   local c = idxf:read (4)
+   -- print (string.format ("c: %q", c))
+   local c1, c2, c3, c4 = c:byte (1, 4)
+
+   -- if c1 < 0 then c1 = 256 + c1 end
+   -- if c2 < 0 then c2 = 256 + c2 end
+   -- if c3 < 0 then c3 = 256 + c3 end
+   -- if c4 < 0 then c4 = 256 + c4 end
+
+   local pos = c1 * 0x1000000 + c2 * 0x10000 + c3 * 0x100 + c4
+
+   return pos
+end
+
+function get_str (pos, srcf, len)
+   srcf:seek ("set", pos)
+   local str = srcf:read (len)
+
+   return str
+end
+
 function lower_bound (idxf, srcf, size, word)
    local m
    local n1, n2 = 1, size + 1
@@ -31,18 +54,12 @@ function lower_bound (idxf, srcf, size, word)
       assert (n1 <= m)
       assert (n2 > m)
 
-      idxf:seek ("set", (m - 1) * 4)
-      local c = idxf:read (4)
-      print (string.format ("c: %q", c))
-      local c1, c2, c3, c4 = c:byte (1, 4)
-      local pos = c1 * 0x1000000 + c2 * 0x10000 + c3 * 0x100 + c4
+      local pos = get_str_pos (idxf, m)
+      local str = get_str (pos, srcf, len)
 
-      srcf:seek ("set", pos)
-      local str = srcf:read (len)
+      -- print (string.format ("str: %s, word: %s, pos: %d", str, word, pos))
 
-      print (string.format ("word: %s, pos: %d", word, pos))
-
-      if str < word then
+      if str:lower () < word:lower () then
          n1 = m
       else
          n2 = m
@@ -54,20 +71,21 @@ function lower_bound (idxf, srcf, size, word)
       end
    end
 
+
    return n1
 end
 
 function get_position_lua (idxf, i)
    idxf:seek ("set", i * 4)
    local c = idxf:read (4)
-   print (string.format ("c: %q", c))
+   -- print (string.format ("c: %q", c))
    local c1, c2, c3, c4 = c:byte (1, 4)
    local pos = c1 * 0x1000000 + c2 * 0x10000 + c3 * 0x100 + c4
 
    return pos
 end
 
-function search_lua (idxfile, srcfile, word)
+function search_on_file (idxfile, srcfile, word)
    -- binary search
    local idxf = io.open (idxfile)
    local srcf = io.open (srcfile)
@@ -76,13 +94,12 @@ function search_lua (idxfile, srcfile, word)
    local lb = lower_bound (idxf, srcf, size, word)
    local ub = lower_bound (idxf, srcf, size, word .. "\255")
 
-   print (string.format ("lb: %d, ub: %d", lb, ub))
+   -- print (string.format ("lb: %d, ub: %d", lb, ub))
 
    local results = {}
 
    local i = lb
    while i < ub do
-      -- local pos = sufarr.get_position (idx, i)
       local pos = get_position_lua (idxf, i)
       local p = math.max (0, pos - 5)
 
@@ -105,6 +122,9 @@ end
 function search (srcfile, word)
    local lb = sufarr.search_lower_bound (idx, word)
    local ub = sufarr.search_upper_bound (idx, word)
+
+   -- print (string.format ("lb: %d, ub: %d", lb, ub))
+
    local f = io.open (srcfile)
    local results = {}
 
--- test/index_test.lua
+++ test/index_test.lua
@@ -4,4 +4,4 @@ assert (loadfile "./indexer.lua") ()
 
 mkindex ("test/idx", "indexer.lua")
 -- loadindex ("test/idx", "indexer.lua")
-print (table.concat ({search_lua ("test/idx", "indexer.lua", "fun")}, "\n"))
+print (table.concat ({search_on_file ("test/idx", "indexer.lua", "fun")}, "\n"))
