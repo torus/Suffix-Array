1cf2f1efe6e75a81a2dad6ae699dcaa2f9964f3b
Toru Hisai <toru-github@torus.jp>
2010-02-27 00:15:59 -1000

    added kjv.idx

--- mkindex.lua
+++ mkindex.lua
@@ -2,3 +2,4 @@ require "sufarr"
 assert (loadfile "./indexer.lua") ()
 
 mkindex ("xcode/Resources/kjv_mini.idx", "xcode/Resources/kjv_mini.txt")
+mkindex ("xcode/Resources/kjv.idx", "xcode/Resources/kjv.txt")
